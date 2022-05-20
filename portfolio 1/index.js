257f00a5e96eff513cfe315b4ea928ccd488c07f
Ilavbare Abdulgafar <101602050+topdawgafar@users.noreply.github.com>
2022-05-20 17:05:23 +0100

    Add files via upload

---
+++ portfolio 1/index.js
@@ -0,0 +1,27 @@
+function validation() {
+    let form = document.getElementById('form')
+    let email = document.getElementById('email').value
+
+let text = document.getElementById('text')
+let pattern = /^[^ ]+\.[a-z]{2,3}$/
+if (email.match(pattern)) {
+    form.classList.add('valid')
+    form.classList.remove('invalid')
+    text.innerHTML = "Your Email Address is Valid"
+
+    text.style.color = '#00ff00'
+}else {
+    form.classList.remove('valid')
+    form.classList.add('invalid')
+    text.innerHTML = "Please Enter A Valid Email Address"
+    text.style.color = '#ff0000'
+    text.style.fontSize = '1rem'
+}
+if (email == '') {
+    form.classList.remove('valid')
+    form.classList.remove('invalid')
+    text.innerHTML = ""
+    text.style.color = '00ff00'
+    text.style.fontSize = '1rem'
+}
+}
